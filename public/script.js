134609a799f84352b6a4da2dbf1568654295c2a3
Chad Erdwins <cerdwins@eitccorp.com>
2019-04-12 12:31:38 -0500

    added cart functionality and total calculations

--- public/script.js
+++ public/script.js
@@ -1,1 +1,20 @@
-console.log('It works.');
+new Vue ({
+    el: "#app",
+    data: {
+        total: 0,
+        items: [
+            {title: "Item1"},
+            {title: "Item2"},
+            {title: "Item3"}
+        ],
+        cart: []
+    },
+    methods: {
+        addItem(index){
+            this.total += 9.99;
+            this.cart.push(this.items[index]);
+            console.log(this.cart.length);
+
+        }
+    }
+});
